24ee27080d39a7aa6efedb127863d439375c13b0
Luiz Felipe Vicente Nina Huallpa <luiz.huallpa@gmail.com>
2022-12-27 20:29:37 -0300

    Adding notations and creating the migrations

--- ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/Models/Categoria.cs
@@ -1,8 +1,24 @@
-﻿namespace ApiCatalogo.Models;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ApiCatalogo.Models;
+
+[Table("Categorias")]
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    [Key]
     public int CategoriaId { get; set; }
+    [Required(ErrorMessage = "Name is requited")]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required(ErrorMessage = "Image link is requited")]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
 }
